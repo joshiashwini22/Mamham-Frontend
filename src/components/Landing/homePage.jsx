f68fd7f752d1b04a133a1c8230dced6cfa6e96fb
Ashwini Joshi <joshiashwini222@gmail.com>
2024-01-31 00:41:56 +0545

    fixes for home page

--- src/components/Landing/homePage.jsx
+++ src/components/Landing/homePage.jsx
@@ -14,15 +14,15 @@ function HomePage() {
               className=" w-full object-cover h-screen"
             />
         </div>
-        <div className="absolute inset-0 flex items-center justify-center">
+        <div className="absolute inset-0 flex items-center justify-end">
           <div className="text-white text-center">
             <div className="text-center mt-8">
-              <span className="text-orange-500 font-poppins text-4xl">One Stop for Ghar ko Khana</span>
-              <span className="text-gray-400 block text-7xl font-poppins">Aja K Khane...</span>
+              <span className="text-700 block text-7xl font-poppins py-4 " style={{color: '#E7E0D2'}}>Aja K Khane...</span>
+              <p className="text-orange-500 font-poppins block text-4xl">One Stop for Ghar ko Khana</p>
             </div>
-            <div className="flex justify-center mt-8">
-              <div className="bg-red-700 text-white font-poppins text-2xl px-8 py-4 rounded-t-xl w-full text-center">Customize your Meal</div>
-              <div className="bg-red-700 text-white font-poppins text-2xl px-8 py-4 rounded-t-xl w-full text-center">Subscription Plans</div>
+            <div className="flex justify-center mt-8 mx-8">
+              <div className="bg-red-700 text-white font-poppins text-l px-8 py-4 mx-8 rounded text-center">Subscription Plans</div>
+              <div className="bg-red-700 text-white font-poppins text-l px-8 py-4 mx-8 rounded text-center">Customize your Meal</div>
             </div>
           </div>
         </div>
